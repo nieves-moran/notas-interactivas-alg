65dc068b10bcfbfb63b26b2a7acb6b4682492973
nieves <nieves@ciencias.unam.mx>
2022-04-05 08:42:57 -0500

    Separar el código del AGPM en clases

--- greedy/agpm_alg.py
+++ greedy/agpm_alg.py
@@ -63,7 +63,7 @@ class Circulo:
     anot = None
     valor = None
 
-class Envir:
+class Env:
     vars = dict()  
 
 class Union_find: 
@@ -87,54 +87,39 @@ class Union_find:
         while(self.parent[i] != i): 
             i = self.parent[i]
         return i
-#Todo el código va a estar en una clase, esto porque 
-#si lo divido en varias clases no se puede realizar la conexión con el mouse 
-class AGPM: 
-    ax = None 
-    fig = None 
-    g = None 
-    cid = None 
-    box = None
-    rad = 1
-    ind = 0
-    #va a ser el máximo x de la gráfica 
-    max_x_g = float('-inf')
-    max_y_g = float('-inf')
-    def __init__(self): 
-        self.config_imagen() 
-        self.config_botones()
-        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick_pos_vert)
-        self.g = Grafica()
-    @out1.capture()
-    def onclick_pos_vert(self,event): 
-        #poner los vertices 
-        c = Circle((event.xdata,event.ydata),radius = self.rad,facecolor = 'white',edgecolor = 'black')
-        self.max_x_g = max(self.max_x_g, event.xdata + self.rad)
-        self.max_y_g = max(self.max_y_g, event.ydata + self.rad)
-        anot = self.ax.annotate("{}".format(self.ind), (event.xdata,event.ydata),color='black', weight='bold', fontsize=10, ha='center', va='center') 
-        self.ax.add_patch(c)
-        cr = Circulo() 
-        cr.pos = [event.xdata,event.ydata] 
-        cr.img = c 
-        self.g.pos_nodos[self.ind] = cr
-        self.ind = self.ind + 1
-        self.ax.relim()
-        self.ax.autoscale_view()  
-    def config_imagen(self): 
-        self.fig,self.ax =  plt.subplots()  
-        plt.gca().set_aspect('equal', adjustable='box')
-        #poner 4 puntos en donde quieres 
-        c = Circle((20,20),radius = 1, visible = False)
-        self.ax.add_patch(c)
-        c = Circle((0,0),radius = 1, visible = False)
-        self.ax.add_patch(c)
-        self.ax.relim()
-        self.ax.autoscale_view() 
-        plt.axis('off')
-    def boton_listo_handler_vert(self,event): 
-        #pasar al siguiente estado 
-        self.fig.canvas.mpl_disconnect(self.cid) 
-        self.config_obtener_aristas()
+def agregar_arista(u,v):
+        #si ya está, no la agregues 
+        if( not ((v in env.vars['g'].ady and u in env.vars['g'].ady[v]) or (u in env.vars['g'].ady and v in env.vars['g'].ady[u]))): 
+            xu,yu = env.vars['g'].pos_nodos[u].img.get_center() 
+            xv,yv = env.vars['g'].pos_nodos[v].img.get_center() 
+            linea = PathPatch(Path([inter_points(env.vars['rad'],xu,yu,xv,yv),inter_points(env.vars['rad'],xv,yv,xu,yu)]), facecolor='none', edgecolor='black')
+            env.vars['ax'].add_patch(linea)
+            xm,ym = punto_medio(xu,yu,xv,yv,0.5)
+            anot = env.vars['ax'].annotate("10", (xm,ym),color='black', weight='bold', fontsize=10, ha='center', va='center') 
+            #agregar a la grafica 
+            n = Nodo()
+            if u not in env.vars['g'].ady: 
+                env.vars['g'].ady[u] = dict()
+            if v not in env.vars['g'].ady[u]: 
+                env.vars['g'].ady[u][v] = n
+
+            if v not in env.vars['g'].ady: 
+                env.vars['g'].ady[v] = dict()
+            if u not in env.vars['g'].ady[v]: 
+                env.vars['g'].ady[v][u] = n
+            #de un lado 
+            env.vars['g'].ady[u][v].arista = (u,v)
+            env.vars['g'].ady[u][v].peso = 10
+            env.vars['g'].ady[u][v].linea = linea 
+            env.vars['g'].ady[u][v].anot = anot
+            #para el otro 
+            env.vars['g'].ady[v][u].arista = (u,v)
+            env.vars['g'].ady[v][u].peso = 10 
+            env.vars['g'].ady[v][u].linea = linea 
+            env.vars['g'].ady[v][u].anot = anot
+            #agregar el widget 
+    
+class Grafica_aleatoria: 
     def generar_aristas_aleatorias(self,n): 
         union_f = Union_find(n*n)
         aristas = [] 
@@ -162,54 +147,105 @@ class AGPM:
         #una cuadricula de nodos de nxn 
         for y in range(0,n): 
             for x in range(0,n): 
-                c = Circle((x*self.rad*4,y*self.rad*4),radius = self.rad,facecolor = 'white',edgecolor = 'black')
+                c = Circle((x*env.vars['rad']*4,y*env.vars['rad']*4),radius = env.vars['rad'],facecolor = 'white',edgecolor = 'black')
                 cr = Circulo() 
-                anot = self.ax.annotate("{}".format(self.ind ), (x*self.rad*4,y*self.rad*4),color='black', weight='bold', fontsize=10, ha='center', va='center') 
-                self.ax.add_patch(c)
-                cr.pos = [y*self.rad*4,y*self.rad*4] 
+                anot = env.vars['ax'].annotate("{}".format(env.vars['ind']), (x*env.vars['rad']*4,y*env.vars['rad']*4),color='black', weight='bold', fontsize=10, ha='center', va='center') 
+                env.vars['ax'].add_patch(c)
+                cr.pos = [y*env.vars['rad']*4,y*env.vars['rad']*4] 
                 cr.img = c 
-                self.g.pos_nodos[self.ind] = cr
-                self.ind = self.ind + 1
-                self.max_x_g = max(self.max_x_g,c.get_center()[0] + 2*self.rad)
-                self.max_y_g = max(self.max_y_g,c.get_center()[1] + 2*self.rad)
-        self.ax.relim()
-        self.ax.autoscale_view()
-    def boton_aleatoria_handler_vert(self,event): 
+                env.vars['g'].pos_nodos[env.vars['ind']] = cr
+                env.vars['ind'] = env.vars['ind'] + 1
+                env.vars['max_x_g'] = max(env.vars['max_x_g'],c.get_center()[0] + 2*env.vars['rad'])
+                env.vars['max_y_g'] = max(env.vars['max_y_g'],c.get_center()[1] + 2*env.vars['rad'])
+        env.vars['ax'].relim()
+        env.vars['ax'].autoscale_view()
+    def __init__(self): 
         #poner nodos
         self.poner_nodos(4)
         ars_al = self.generar_aristas_aleatorias(4) 
         for (u,v,p) in ars_al: 
-            self.agregar_arista(u,v,False)
-            self.g.ady[u][v].anot.set(text = "{}".format(p))
-            self.g.ady[u][v].peso = p 
-        # pasar a la configuracion de ejecucion  
-        self.config_ejecucion() 
+            agregar_arista(u,v)
+            env.vars['g'].ady[u][v].anot.set(text = "{}".format(p))
+            env.vars['g'].ady[u][v].peso = p 
+        env.vars['e3'] = Ejecucion()
+    def vecinos(self,v,n):
+            #la grafia es de nxn y v es el vertice 
+            (x,y) = v // n  , v % n
+            v = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)] 
+            vp = []
+            for (x,y) in v: 
+                if (0 <= x < n and 0 <= y < n): 
+                    vp.append(x*n + y)
+            return vp 
+class Obtener_vertices:  
+    def config_imagen(self): 
+        plt.gca().set_aspect('equal', adjustable='box')
+        #poner 4 puntos en donde quieres 
+        c = Circle((20,20),radius = 1, visible = False)
+        env.vars['ax'].add_patch(c)
+        c = Circle((0,0),radius = 1, visible = False)
+        env.vars['ax'].add_patch(c)
+        env.vars['ax'].relim()
+        env.vars['ax'].autoscale_view() 
+        plt.axis('off')
+    def boton_listo_handler(self,event): 
+        #pasar al siguiente estado 
+        env.vars['fig'].canvas.mpl_disconnect(env.vars['cid']) 
+        env.vars['e1'] = Obtener_aristas()
+    def boton_aleatoria(self,event): 
+        env.vars['fig'].canvas.mpl_disconnect(env.vars['cid']) 
+        env.vars['e2'] = Grafica_aleatoria()
+    def boton_entrada_usr_handler(self,event): 
+        env.vars['cid'] = env.vars['fig'].canvas.mpl_connect('button_press_event', self.onclick)
+        #quitar el boton de aleatoria 
+        boton_listo = widgets.Button(description='Listo')
+        boton_listo.on_click(self.boton_listo_handler)  
+        env.vars['box'].children = [boton_listo]
+    def config_botones(self): 
+        boton_ent_usr = widgets.Button(description='Entrada de usuario')
+        boton_ent_usr.on_click(self.boton_entrada_usr_handler)  
+        boton_aleatoria = widgets.Button(description='Generar aleatoria')
+        boton_aleatoria.on_click(self.boton_aleatoria)  
+        env.vars['box'] = widgets.VBox([boton_ent_usr,boton_aleatoria])
+        display(env.vars['box'])
+    @out1.capture()
+    def onclick(self,event): 
+        #poner los vertices 
+        c = Circle((event.xdata,event.ydata),radius = env.vars['rad'],facecolor = 'white',edgecolor = 'black')
+        env.vars['max_x_g'] = max(env.vars['max_x_g'], event.xdata + env.vars['rad'])
+        env.vars['max_y_g'] = max(env.vars['max_y_g'], event.ydata + env.vars['rad'])
+        anot = env.vars['ax'].annotate("{}".format(env.vars['ind']), (event.xdata,event.ydata),color='black', weight='bold', fontsize=10, ha='center', va='center') 
+        env.vars['ax'].add_patch(c)
+        cr = Circulo() 
+        cr.pos = [event.xdata,event.ydata] 
+        cr.img = c 
+        env.vars['g'].pos_nodos[env.vars['ind']] = cr
+        env.vars['ind'] = env.vars['ind'] + 1
+        env.vars['ax'].relim()
+        env.vars['ax'].autoscale_view()  
+    def __init__(self): 
+        print("se llama a obtener vertices")
+        self.config_imagen() 
+        self.config_botones()
+        env.vars['g'] = Grafica()
+class Obtener_aristas: 
+    extremos = [] 
+    def __init__(self):
+        env.vars['cid'] = env.vars['fig'].canvas.mpl_connect('button_press_event', self.onclick)
+        self.config_botones()
+    #salida 
+    def boton_listo_handler(self,event): 
+        #pasar al siguiente que es escoger un inicial 
+        env.vars['fig'].canvas.mpl_disconnect(env.vars['cid']) 
+        env.vars['e3'] = Ejecucion()
+        #self.config_ejecucion()
     def config_botones(self): 
         boton_listo = widgets.Button(description='listo')
-        boton_listo.on_click(self.boton_listo_handler_vert)  
-        boton_aleatoria = widgets.Button(description='Generar aleatoria')
-        boton_aleatoria.on_click(self.boton_aleatoria_handler_vert)  
-        self.box = widgets.VBox([boton_listo,boton_aleatoria])
-        display(self.box)
-
-##-------------------funciones de la parte para obtener aristas-----------------------------
-    #elegidos para formar arista 
-    extremos = [] 
-    def config_obtener_aristas(self):
-        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick_aristas)
-        self.config_botones_aristas()
-    #salida 
-    def boton_listo_handler_arist(self,event): 
-        #pasar al siguiente que es escoger un inicial 
-        self.fig.canvas.mpl_disconnect(self.cid) 
-        self.config_ejecucion()
-    def config_botones_aristas(self): 
-        boton_listo = widgets.Button(description='listo')
-        boton_listo.on_click(self.boton_listo_handler_arist)  
-        self.box.children = [boton_listo]
+        boton_listo.on_click(self.boton_listo_handler)  
+        env.vars['box'].children = [boton_listo]
     @out1.capture()
-    def onclick_aristas(self,event): 
-        for i,cir in self.g.pos_nodos.items(): 
+    def onclick(self,event): 
+        for i,cir in env.vars['g'].pos_nodos.items(): 
             x,y = cir.pos
             r = cir.img.get_radius()
             if(math.sqrt((x - event.xdata)**2) <= r and math.sqrt((y - event.ydata)**2) <= r ): 
@@ -217,104 +253,61 @@ class AGPM:
                 if (i not in self.extremos): 
                     self.extremos.append(i)
                     if(len(self.extremos) == 2): 
-                        self.agregar_arista(self.extremos[0],self.extremos[1],True)
+                        agregar_arista(self.extremos[0],self.extremos[1])
+                        self.agregar_widget_peso(self.extremos[0],self.extremos[1])
+                        for i in self.extremos: 
+                            env.vars['g'].pos_nodos[i].img.set(facecolor = 'white')
+                        self.extremos = []
                 else:
                     self.extremos.remove(i)
                     cir.img.set(facecolor = 'white')
 
     def esc_peso_handler(self,change):
         u,v = eval(change.owner.description) 
-        self.g.ady[u][v].anot.set(text = "{}".format(change.new))
-        self.g.ady[u][v].peso = change.new 
+        env.vars['g'].ady[u][v].anot.set(text = "{}".format(change.new))
+        env.vars['g'].ady[u][v].peso = change.new 
     def agregar_widget_peso(self,u,v): 
-        ch = list(self.box.children)
+        ch = list(env.vars['box'].children)
         esc_peso = widgets.BoundedIntText(value=10,min=0,max=100,step=1,description='({},{})'.format(u,v),disabled=False)
         esc_peso.observe(self.esc_peso_handler, names='value')
         ch.append(esc_peso)
-        self.box.children = tuple(ch)
-    def agregar_arista(self,u,v,widget):
-        #si ya está, no la agregues 
-        if( not ((v in self.g.ady and u in self.g.ady[v]) or (u in self.g.ady and v in self.g.ady[u]))): 
-            xu,yu = self.g.pos_nodos[u].img.get_center() 
-            xv,yv = self.g.pos_nodos[v].img.get_center() 
-            linea = PathPatch(Path([inter_points(self.rad,xu,yu,xv,yv),inter_points(self.rad,xv,yv,xu,yu)]), facecolor='none', edgecolor='black')
-            self.ax.add_patch(linea)
-            xm,ym = punto_medio(xu,yu,xv,yv,0.5)
-            anot = self.ax.annotate("10", (xm,ym),color='black', weight='bold', fontsize=10, ha='center', va='center') 
-            #agregar a la grafica 
-            n = Nodo()
-            if u not in self.g.ady: 
-                self.g.ady[u] = dict()
-            if v not in self.g.ady[u]: 
-                self.g.ady[u][v] = n
-
-            if v not in self.g.ady: 
-                self.g.ady[v] = dict()
-            if u not in self.g.ady[v]: 
-                self.g.ady[v][u] = n
-            #de un lado 
-            self.g.ady[u][v].arista = (u,v)
-            self.g.ady[u][v].peso = 10
-            self.g.ady[u][v].linea = linea 
-            self.g.ady[u][v].anot = anot
-            #para el otro 
-            self.g.ady[v][u].arista = (u,v)
-            self.g.ady[v][u].peso = 10 
-            self.g.ady[v][u].linea = linea 
-            self.g.ady[v][u].anot = anot
-            #agregar el widget 
-            if(widget): 
-                self.agregar_widget_peso(u,v)
-        for i in self.extremos: 
-            self.g.pos_nodos[i].img.set(facecolor = 'white')
-        self.extremos = []
-    def vecinos(self,v,n):
-        #la grafia es de nxn y v es el vertice 
-        (x,y) = v // n  , v % n
-        v = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)] 
-        vp = []
-        for (x,y) in v: 
-            if (0 <= x < n and 0 <= y < n): 
-                vp.append(x*n + y)
-        return vp 
-
+        env.vars['box'].children = tuple(ch)
     
-#--------------------------------------------ejecucion del algoritmo -------------------------
-    #indice de la arista 
+class Ejecucion: 
     ind_ar = 0 
     ar_ord = None 
     union_f = None
     array_col = None 
     color_map = None
     anotacion_g = None 
-    def config_boton_listo_ej(self): 
+    def config_boton(self): 
         boton_listo = widgets.Button(description='siguiente')
         boton_listo.on_click(self.boton_listo_handler_ej)  
-        self.box.children = [boton_listo]
+        env.vars['box'].children = [boton_listo]
     def obtener_aristas_ord(self): 
         ars = [] 
         ars_sp = [] 
-        for i in self.g.ady.keys(): 
-            for j in self.g.ady[i].keys(): 
+        for i in env.vars['g'].ady.keys(): 
+            for j in env.vars['g'].ady[i].keys(): 
                 if((i,j) not in ars_sp): 
                     ars_sp.append((i,j))
                     ars_sp.append((j,i))
-                    ars.append((i,j,self.g.ady[i][j].peso))
+                    ars.append((i,j,env.vars['g'].ady[i][j].peso))
         ars.sort(key = lambda x : x[2])
         return ars
     def colorear_vert(self): 
-        for i in range(0,self.ind): 
-            vert = self.g.pos_nodos[i]
+        for i in range(0,env.vars['ind']): 
+            vert = env.vars['g'].pos_nodos[i]
             vert.img.set(facecolor = self.array_col[i])
     #entrada 
-    def config_ejecucion(self): 
-        self.config_boton_listo_ej()
-        self.union_f = Union_find(self.ind)
+    def __init__(self): 
+        self.config_boton()
+        self.union_f = Union_find(env.vars['ind'])
         self.ar_ord = self.obtener_aristas_ord()
         self.color_map = plt.get_cmap('tab20c', 1000)
         #para el número de colores 
-        frac = 1/self.ind
-        self.array_col = [self.color_map((i+1)*frac) for i in range(0,self.ind)]
+        frac = 1/env.vars['ind']
+        self.array_col = [self.color_map((i+1)*frac) for i in range(0,env.vars['ind'])]
         self.anotacion_g  = Anot_grupos()
         self.colorear_vert()
     def limpiar_anotacion_grupos(self): 
@@ -324,26 +317,23 @@ class AGPM:
         self.anotacion_g.renglones = []
     def actualiza_anot_grupos(self): 
         grupos = dict() 
-        #esta en self.max_x_g 
-        x = self.max_x_g + self.rad 
-        y = self.max_y_g 
+        #esta en env.vars['max_x_g'] 
+        x = env.vars['max_x_g'] + env.vars['rad'] 
+        y = env.vars['max_y_g'] 
         self.limpiar_anotacion_grupos() 
         #crear los grupos de colores 
-        print("son {} pos_nodos".format(len(self.g.pos_nodos)))
-        for i in range(0,len(self.g.pos_nodos)):
+        for i in range(0,len(env.vars['g'].pos_nodos)):
             if(self.union_f.find(i) not in grupos ): 
                 grupos[self.union_f.find(i)] = [] 
             grupos[self.union_f.find(i)].append(i)
-        print("grupos tiene {}".format(len(grupos.items())))
         for j,g in grupos.items(): 
-            r = Rectangle((x,y),width = self.rad,height = 1,facecolor = self.array_col[j],edgecolor = 'black') 
-            self.ax.add_patch(r)
-            print("las coordenadas son {}".format(r.get_xy()))
+            r = Rectangle((x,y),width = env.vars['rad'],height = 1,facecolor = self.array_col[j],edgecolor = 'black') 
+            env.vars['ax'].add_patch(r)
             ren = Renglon() 
             ren.cuadro_color = r
-            ren.anotacion = self.ax.text(x +3*self.rad,y,"{}".format(g))
+            ren.anotacion = env.vars['ax'].text(x +3*env.vars['rad'],y,"{}".format(g))
             self.anotacion_g.renglones.append( ren)
-            y = y - 2*self.rad  
+            y = y - 2*env.vars['rad']  
     def boton_listo_handler_ej(self,event): 
         if(self.ind_ar == len(self.ar_ord)):
             return
@@ -357,19 +347,26 @@ class AGPM:
             if(self.union_f.size[pu] < self.union_f.size[pv]): 
                 p = pv
             self.union_f.unite(u,v)
-            for i in range(0,self.ind): 
+            for i in range(0,env.vars['ind']): 
                 if(self.union_f.find(i) == p): 
-                    self.g.pos_nodos[i].img.set(facecolor = self.array_col[p])
-            self.g.ady[u][v].linea.set(linewidth = 3)
+                    env.vars['g'].pos_nodos[i].img.set(facecolor = self.array_col[p])
+            env.vars['g'].ady[u][v].linea.set(linewidth = 3)
         else: 
-            self.g.ady[u][v].linea.set(linestyle = '--')
-            self.g.ady[u][v].anot.set(visible = False)
-            self.g.ady[u][v].linea.set(visible = False)
+            env.vars['g'].ady[u][v].linea.set(linestyle = '--')
+            env.vars['g'].ady[u][v].anot.set(visible = False)
+            env.vars['g'].ady[u][v].linea.set(visible = False)
         self.actualiza_anot_grupos()
-        self.ax.relim()
-        self.ax.autoscale_view()
+        env.vars['ax'].relim()
+        env.vars['ax'].autoscale_view()
         self.ind_ar = self.ind_ar + 1
+env = Env() 
+env.vars['fig'],env.vars['ax'] = plt.subplots()  
+env.vars['cid'] = None
+env.vars['box'] = None
+env.vars['max_x_g'] = float('-inf')
+env.vars['max_y_g'] = float('-inf')
+env.vars['g'] = None
+env.vars['ind'] = 0
+env.vars['rad'] = 1
 
-
-
-estado = AGPM() 
+estado = Obtener_vertices() 
