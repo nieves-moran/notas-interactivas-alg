b8551a779a96cd61e0107208dca3be038a490539
nieves <nieves@ciencias.unam.mx>
2022-04-12 16:42:48 -0500

    Arreglar el problema de que borraba todo
    Ahora borra solo el arbol

--- adicionales/trie_alg.py
+++ adicionales/trie_alg.py
@@ -38,7 +38,7 @@ class Anotacion:
         self.text = env.vars['ax'].text(x, y,text,fontsize = 9)
         #con el tamano de 9 caben diez  
         nc = len(text)//10 + 1 
-        self.rect = Rectangle((x,y),width = nc,height = 0.2,visible = True) 
+        self.rect = Rectangle((x,y),width = nc,height = 0.2,visible = False) 
         env.vars['ax'].add_patch(self.rect) 
         env.vars['ax'].relim()
         env.vars['ax'].autoscale()
@@ -92,7 +92,7 @@ def dfs(n,arbol,prof,y):
     if(not n.hijos): 
         prof[n] = y 
     for i in n.hijos : 
-        dfs(i,arbol,prof,y - 3*env.vars['rad'])
+        dfs(i,arbol,prof,y - 4*env.vars['rad'])
 
 def prof_hojas(arbol): 
     prof = dict() 
@@ -107,8 +107,20 @@ def entre_hijos(n):
     xp,_ = ph.circ.get_center()
     xu,_ = uh.circ.get_center()
     return (xp + xu) /2 
-
+def limpiar_arbol(): 
+    arbol = env.vars['arbol']
+    for _,v in arbol.vertices.items(): 
+        if(v.circ != None): 
+            v.circ.set(visible = False)
+        v.circ = None 
+        for u in v.hijos: 
+            if((v,u) in arbol.aristas): 
+                if(arbol.aristas[(v,u)].linea != None): 
+                    arbol.aristas[(v,u)].linea.set(visible = False)
+                    arbol.aristas[(v,u)].anot.set(visible = False)
+                arbol.aristas[v,u].linea = None 
 def dibujar_arbol():  
+    limpiar_arbol() 
     rad = env.vars['rad']
     arbol = env.vars['arbol']
     prof_h = prof_hojas(arbol) 
@@ -118,7 +130,7 @@ def dibujar_arbol():
         v.circ = None 
     for h,p in prof_h.items():
         cola.append((h,x,p))
-        x = x + 3*rad
+        x = x + 4*rad
     hijos_proc = dict()
     while(cola): 
         n,x,y = cola[0]
@@ -132,7 +144,7 @@ def dibujar_arbol():
                 hijos_proc[p] = 0 
             hijos_proc[p] = hijos_proc[p] + 1
             if(hijos_proc[p] == len(p.hijos)): 
-                cola.append((p,entre_hijos(p),y + 3*rad)) 
+                cola.append((p,entre_hijos(p),y + 4*rad)) 
     for i,v in arbol.vertices.items(): 
         for j in range(0,128): 
             if(v.alf[j] != -1):  
@@ -150,6 +162,7 @@ class Ejecucion:
     nodo_act = 0 
     ind = 0
     cad_entrada = ""
+    ars_color = [] 
     def crear_arbol(self,ars,n):
         arbol = env.vars['arbol']
         for i in range(0,n): 
@@ -167,6 +180,11 @@ class Ejecucion:
     def zoom_menos(self): 
         x,y = env.vars['fig'].get_size_inches()
         env.vars['fig'].set_size_inches(x-1,y-1)
+    def colorear_aristas(self): 
+        arbol = env.vars['arbol']
+        for (v,u)  in self.ars_color:
+            arbol.aristas[(v,u)].linea.set(color = 'red')
+            arbol.aristas[(v,u)].linea.set(linewidth = 3)
     def siguiente_paso(self):       
         arbol = env.vars['arbol']
         if(self.ind == len(self.cad_entrada)): 
@@ -175,11 +193,16 @@ class Ejecucion:
             self.nodo_act = arbol.raiz 
             self.ind = 0 
             self.cad_entrada = ""
+            self.ars_color = [] 
             return
         if(self.nodo_act.alf[ord(self.cad_entrada[self.ind])] != -1): 
+            #print("esta en el arbol")
+            self.ars_color.append((self.nodo_act,self.nodo_act.alf[ord(self.cad_entrada[self.ind])]))
             self.nodo_act = self.nodo_act.alf[ord(self.cad_entrada[self.ind])]
             #pintarlo de rojo 
+            #print("agrega a {}".format(self.nodo_act.alf[ord(self.cad_entrada[self.ind])])))
         else: 
+            #print("no esta en el arbol")
             nuevo = Vertice() 
             #info del nuevo 
             arbol.vertices[arbol.n_nodos] = nuevo  
@@ -188,14 +211,16 @@ class Ejecucion:
             nuevo.valor = arbol.n_nodos 
             #info del padre  
             self.nodo_act.alf[ord(self.cad_entrada[self.ind])] = nuevo
+            self.ars_color.append((self.nodo_act,nuevo)) 
             self.nodo_act.hijos.append(nuevo)
             #info aristas 
+            #print("agrega a {}".format(nuevo))
             arbol.aristas[(self.nodo_act,nuevo)] = Arista() 
             #info del arbol 
             arbol.n_nodos = arbol.n_nodos + 1
             self.nodo_act = nuevo
-            env.vars['ax'].clear()
-            dibujar_arbol() 
+        dibujar_arbol()
+        self.colorear_aristas()
         self.ind = self.ind + 1    
         #eliminar arbol 
     @out1.capture() 
@@ -236,12 +261,11 @@ class Ejecucion:
         if((x - event.xdata)**2 + (y - event.ydata)**2 <= env.vars['rad']):
             self.config_teclas_entrada()  
     def config_mouse(self): 
-        print("config mouse")
         env.vars['cid_m'] = env.vars['fig'].canvas.mpl_connect('button_press_event', self.handler_mouse)
-        print(env.vars['cid_m'])
                 
     def config_imagen(self): 
         plt.gca().set_aspect('equal', adjustable='box')
+        plt.axis('off')
     def config_teclas_inicial(self): 
         if(env.vars['cid_t'] != None): 
             env.vars['fig'].canvas.mpl_disconnect(env.vars['cid_t']) 
@@ -268,7 +292,7 @@ class Ejecucion:
     def __init__(self): 
         print("inicio")
         ars = {0:[('a',1),('b',2),('d',8)],1:[('a',3)],2:[('b',4),('c',5),('x',6),('m',7)]}
-        #nodo inicial razi 
+        #nodo inicial raiz 
         self.nodo_act = env.vars['arbol'].raiz
         #poner anotacion 
         self.entrada_anot  = Anotacion(0,1.5*env.vars['rad'],"Entrada:")  
