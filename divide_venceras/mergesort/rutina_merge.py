13e7aef57af3afe30faf482d6e6448dd7216b642
nieves <nieves@ciencias.unam.mx>
2022-04-20 10:20:00 -0500

    Agregar la implementación de la rutina para combinar de mergesort completa

--- divide_venceras/mergesort/rutina_merge.py
+++ divide_venceras/mergesort/rutina_merge.py
@@ -59,6 +59,12 @@ def dibujar_arreglo(arreglo,x,y,etiq_b):
         arr.elms[i].anot = ax.text(x+1.5,y+1.5,etiq,ha = 'center',va = 'center',fontsize = 9) 
         x = x + 3 
     return arr
+class Ind: 
+    letra = None
+    anot = None 
+    valor = 0 
+    def __init__(self,l): 
+        self.letra = l
 class Env:
     vars = dict()  
 
@@ -66,11 +72,48 @@ class Ejecucion:
     arr1 = None 
     arr2 = None  
     arr3 = None
+    ind_i = None 
+    ind_j = None 
+    ind_k = None
+    anots = dict()
+    def poner_anotaciones(self): 
+        ax = env.vars['ax']
+        self.anots['anot_a1'] = ax.text(15,5,"$A_1$",ha = 'center',va = 'center',fontsize = 11)
+        self.anots['anot_a2'] = ax.text(55,5,"$A_2$",ha = 'center',va = 'center',fontsize = 11)        
+        self.anots['anot_a3'] = ax.text(35,15,"$A_3$",ha = 'center',va = 'center',fontsize = 11)           
     def config_imagen(self): 
         plt.gca().set_aspect('equal', adjustable='box')
-
+        plt.axis('off')
     def siguiente_paso(self):
-        print('ejecuta el siguiente paso')
+        i,j,k = self.ind_i.valor,self.ind_j.valor,self.ind_k.valor
+        if(k == len(self.arr3.elms)): 
+            return 
+        n, m = len(self.arr1.elms),len(self.arr2.elms)
+        if(i == n): 
+            self.arr3.elms[k].valor = self.arr2.elms[j].valor
+            self.arr3.elms[k].anot.set(text = self.arr3.elms[k].valor)
+            self.ind_j.anot.set(x = self.ind_j.anot.get_position()[0] + 3)
+            j = j + 1 
+        elif(j == m): 
+            self.arr3.elms[k].valor = self.arr1.elms[i].valor
+            self.arr3.elms[k].anot.set(text = self.arr3.elms[k].valor)
+            self.ind_i.anot.set(x = self.ind_i.anot.get_position()[0] + 3)
+            i = i + 1       
+        elif(self.arr1.elms[i].valor < self.arr2.elms[j].valor): 
+            self.arr3.elms[k].valor = self.arr1.elms[i].valor
+            self.arr3.elms[k].anot.set(text = self.arr3.elms[k].valor)
+            self.ind_i.anot.set(x = self.ind_i.anot.get_position()[0] + 3)
+            i = i + 1 
+        else: 
+            self.arr3.elms[k].valor = self.arr2.elms[j].valor
+            self.arr3.elms[k].anot.set(text = self.arr3.elms[k].valor)
+            self.ind_j.anot.set(x = self.ind_j.anot.get_position()[0] + 3)
+            j = j + 1 
+        self.ind_k.anot.set(x = self.ind_k.anot.get_position()[0] + 3)
+        k = k + 1
+        self.ind_i.valor = i 
+        self.ind_j.valor = j 
+        self.ind_k.valor = k        
     @out1.capture()
     def teclas_handler(self,event): 
         if(event.key == 'n'): 
@@ -87,19 +130,29 @@ class Ejecucion:
         env.vars['fig'].set_size_inches(x-1,y-1)
     def config_teclas(self): 
         env.vars['cid_t'] = env.vars['fig'].canvas.mpl_connect('key_press_event', self.teclas_handler)
+    def dibujar_indice(self,x,y,l): 
+        ind = Ind(l)
+        ind.anot = env.vars['ax'].text(x,y,l,va = 'center',ha ='center',fontsize = 9)
+        return ind
     def dibujar_arreglos_iniciales(self): 
-        a1 = [random.randint(1,20) for i in range(0,10)]
-        a2 = [random.randint(1,20) for i in range(0,10)]
+        a1 = [random.randint(-20,20) for i in range(0,10)]
+        a2 = [random.randint(-20,20) for i in range(0,10)]
+        a1.sort()
+        a2.sort() 
         a3 = [0 for i in range(0,20)]
         self.arr1 = dibujar_arreglo(a1,0,0,True)
+        self.ind_i = self.dibujar_indice(1.5,-1.5,"i") 
         self.arr2 = dibujar_arreglo(a2,40,0,True)
+        self.ind_j = self.dibujar_indice(41.5,-1.5,"j")
         self.arr3 = dibujar_arreglo(a3,5,10,False)
+        self.ind_k = self.dibujar_indice(6.5,8.5,"k") 
         env.vars['ax'].relim()
         env.vars['ax'].autoscale()
     def __init__(self): 
         self.config_imagen()
         self.config_teclas()
         self.dibujar_arreglos_iniciales()
+        self.poner_anotaciones() 
 
 env = Env() 
 env.vars['fig'],env.vars['ax'] = plt.subplots() 
