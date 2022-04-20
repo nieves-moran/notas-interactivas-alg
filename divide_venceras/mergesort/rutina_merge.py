aea7b1f48608621490a5c89dd86a29aa0cbfe93b
nieves <nieves@ciencias.unam.mx>
2022-04-20 09:20:13 -0500

    Dibujar los arreglos iniciales para la rutina de mergesort

---
+++ divide_venceras/mergesort/rutina_merge.py
@@ -0,0 +1,108 @@
+from cmath import atan
+from glob import glob
+from matplotlib.patches import Circle
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Path
+from matplotlib.patches import PathPatch
+import matplotlib.pyplot as plt
+from IPython.display import display
+import ipywidgets as widgets
+from IPython.display import clear_output
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import random 
+
+import math
+
+from numpy import array
+%matplotlib nbagg
+out1 = widgets.Output()
+display(out1)
+def inter_points(rad,x1,y1,x2,y2): 
+    phi = math.atan2(y2-y1, x2-x1)
+    x = x1 + rad * math.cos(phi)
+    y = y1 + rad * math.sin(phi)
+    return (x,y)
+#punto medio entre dos puntos pero con una separación de s, una fraccion de la linae(frac)
+def punto_medio(x1,y1,x2,y2,s,fra): 
+    dx = x1 - x2 
+    dy = y1 - y2 
+    ang = (math.pi/2 if dy > 0 else (3*math.pi)/2) if dx == 0 else math.atan(dy/dx)
+    ang = ang + 2* math.pi if ang < 0 else ang
+    x = math.sqrt((x1- x2)**2 + (y1 - y2)**2)*fra
+    y = s 
+    xp = x*math.cos(ang) - y*math.sin(ang)
+    yp = x*math.sin(ang) + y*math.cos(ang)
+    ##llegué a la conclusión que tienes que trasladarlo a el que tiene la menor x 
+    xp = xp + (x1 if x1 < x2 else x2) 
+    yp = yp + (y1 if x1 < x2 else y2 )
+    return (xp,yp)
+class Arreglo: 
+    elms = [] 
+    def __init__(self,n): 
+        self.elms = [Celda() for i in range(0,n)]
+
+class Celda: 
+    rect = None 
+    valor = None 
+    anot = None 
+
+def dibujar_arreglo(arreglo,x,y,etiq_b): 
+    ax = env.vars['ax']
+    arr = Arreglo(len(arreglo))
+    for i in range(0,len(arreglo)): 
+        arr.elms[i].rect = Rectangle((x,y),width = 3 , height = 3,facecolor = 'white',edgecolor = 'black') 
+        ax.add_patch(arr.elms[i].rect) 
+        arr.elms[i].valor = arreglo[i]
+        etiq = "{}".format(arreglo[i])
+        if(not etiq_b): 
+            etiq = ""
+        arr.elms[i].anot = ax.text(x+1.5,y+1.5,etiq,ha = 'center',va = 'center',fontsize = 9) 
+        x = x + 3 
+    return arr
+class Env:
+    vars = dict()  
+
+class Ejecucion: 
+    arr1 = None 
+    arr2 = None  
+    arr3 = None
+    def config_imagen(self): 
+        plt.gca().set_aspect('equal', adjustable='box')
+
+    def siguiente_paso(self):
+        print('ejecuta el siguiente paso')
+    @out1.capture()
+    def teclas_handler(self,event): 
+        if(event.key == 'n'): 
+            self.siguiente_paso() 
+        if(event.key == '-'):
+            self.zoom_menos()  
+        elif(event.key == '+'): 
+            self.zoom_mas() 
+    def zoom_mas(self): 
+        x,y = env.vars['fig'].get_size_inches()
+        env.vars['fig'].set_size_inches(x+1,y+1)
+    def zoom_menos(self): 
+        x,y = env.vars['fig'].get_size_inches()
+        env.vars['fig'].set_size_inches(x-1,y-1)
+    def config_teclas(self): 
+        env.vars['cid_t'] = env.vars['fig'].canvas.mpl_connect('key_press_event', self.teclas_handler)
+    def dibujar_arreglos_iniciales(self): 
+        a1 = [random.randint(1,20) for i in range(0,10)]
+        a2 = [random.randint(1,20) for i in range(0,10)]
+        a3 = [0 for i in range(0,20)]
+        self.arr1 = dibujar_arreglo(a1,0,0,True)
+        self.arr2 = dibujar_arreglo(a2,40,0,True)
+        self.arr3 = dibujar_arreglo(a3,5,10,False)
+        env.vars['ax'].relim()
+        env.vars['ax'].autoscale()
+    def __init__(self): 
+        self.config_imagen()
+        self.config_teclas()
+        self.dibujar_arreglos_iniciales()
+
+env = Env() 
+env.vars['fig'],env.vars['ax'] = plt.subplots() 
+env.vars['rad'] = 1 
+env.vars['cid_t'] = None
+env.vars['e1'] = Ejecucion() 
