c67d11991b8bbcb5ece5c578d81ecfabe971d54d
nieves <nieves@ciencias.unam.mx>
2022-05-23 15:57:36 -0500

    Segment-tree: Agregar anotaciones y entrada de usuario.

--- src/adicionales/segment_tree/segment_tree.py
+++ src/adicionales/segment_tree/segment_tree.py
@@ -55,8 +55,10 @@ def dibujar_arbol(arr):
         r = Rectangle((x-env.vars['rad'],y-3*env.vars['rad']),width = 2*env.vars['rad'],height = 2*env.vars['rad'],facecolor = 'white',edgecolor = 'black') 
         env.vars['ax'].add_patch(r)
         cl.rect_anot = env.vars['ax'].text(x, y-2*env.vars['rad'], '${}$'.format(cl.valor),fontsize = 9,ha='center', va='center') 
+        env.vars['ax'].text(x,y-4,i - n,fontsize = 9,va = 'center',ha = 'center')
         x = x - 2*env.vars['rad']
         cl.rect = r 
+        #poner el indice 
     for i in range(n-1,0,-1): 
         cl = Celda() 
         env.vars['seg_t'].arreglo.celdas[i] = cl
@@ -93,7 +95,7 @@ class Segment_tree:
         for i in range(0,n): 
             self.tree[i + n] = arr[i]
         for i in range(n-1,0,-1): 
-            self.tree[i] = self.tree[2*i] + self.tree[2*i+1]  
+            self.tree[i] = min(self.tree[2*i],self.tree[2*i+1])  
 
 class Arreglo: 
     celdas = None 
@@ -203,6 +205,12 @@ class Consulta:
 class Modificacion:  
     hoja = None 
     ind = None
+    casilla_esc = None
+    ax_anot = None 
+    anot = None 
+    nuevo_val = None
+    linea = None 
+    botones = None
     def zoom_mas(self): 
         x,y = env.vars['fig'].get_size_inches()
         env.vars['fig'].set_size_inches(x+1,y+1)
@@ -235,48 +243,80 @@ class Modificacion:
                 self.hoja = i 
         #si hizo click en una hoja la pinta de verde 
         if(self.hoja != -1): 
-            cels[self.hoja].rect.set(facecolor = 'green')
-            cels[self.hoja].rect_anot.set(text = "")
-            self.config_teclas_numero()
-    @out1.capture()
-    def config_teclas_numero(self): 
-            print("se desconecta la tecla")
-            env.vars['fig'].canvas.mpl_disconnect(env.vars['cid_t']) 
-            #desconecta el mouse 
-            env.vars['cid_t'] = env.vars['fig'].canvas.mpl_connect('key_press_event', self.handler_ingresar_numero)
-    @out1.capture()
-    def handler_ingresar_numero(self,event): 
-        if(event.key.isdigit()): 
-            cels = env.vars['seg_t'].arreglo.celdas
-            t = cels[self.hoja].rect_anot.get_text()
-            t = t + event.key
-            cels[self.hoja].rect_anot.set(text = t)
-        elif(event.key == '-'):
-            self.zoom_menos()  
-        elif(event.key == '+'): 
-            self.zoom_mas() 
-        elif(event.key == 'enter'): 
-            self.config_teclas_ej() 
-            return 
+            if(self.casilla_esc != None): 
+                self.casilla_esc.set(facecolor = 'white')
+            cels[self.hoja].rect.set(facecolor = '#009999')
+            self.casilla_esc = cels[self.hoja].rect
     def config_teclas_ej(self): 
         env.vars['fig'].canvas.mpl_disconnect(env.vars['cid_t']) 
             #desconecta el mouse 
         env.vars['cid_t'] = env.vars['fig'].canvas.mpl_connect('key_press_event', self.handler_teclas_ej)
+    def modificar_linea(self,xmin,xmax):
+        y = self.linea.get_path().vertices[0][1]  
+        np = Path([(xmin,y),(xmax,y)])
+        self.linea.set_path(np) 
+    def poner_linea(self,y,xmin,xmax): 
+        l = PathPatch(Path([(xmin,y),(xmax,y)]))
+        l.set(linewidth = 3)
+        l.set(color = '#009999')
+        env.vars['ax'].add_patch(l)
+        return l 
+    def reiniciar(self):
+        #regresar el color de todos
+        for c in env.vars['seg_t'].arreglo.celdas :
+            if(c.rect != None):  
+                c.rect.set(facecolor = 'white')
+            if(c.circ != None): 
+                c.circ.set(facecolor = 'white') 
+        # conectar el mouse 
+        env.vars['cid_m'] = env.vars['fig'].canvas.mpl_connect('button_press_event', self.handler_mouse)
+        # desconectar las teclas
+        env.vars['fig'].canvas.mpl_disconnect(env.vars['cid_t'])  
+        # quitar la linea
+        self.linea.set(visible = False)
+        self.linea = None 
+        # poner los botones 
+        self.poner_botones() 
+        #reiniciar el indice 
+        self.ind = None 
+        self.hoja = None 
+        #poner la anotacion 
+        text= "Escoge alguna casilla haciendo click en ella. Introduce un numero\n"
+        text += "en el cuadro de texto bajo la imagen y presiona el boton cuanto estes listo."
+        self.anot.set(text = text)
+        return 
     def siguiente_paso(self): 
-        print("un paso mas")
+        if(self.ind == 0): 
+            self.reiniciar()
+            return 
         cels = env.vars['seg_t'].arreglo.celdas
         if(self.ind == None): 
             self.ind = self.hoja
-            cels[self.ind].circ.set(facecolor= 'green')
-            cels[self.ind].valor = int(cels[self.ind].rect_anot.get_text()) 
+            cels[self.ind].circ.set(facecolor= '#009999')
+            cels[self.ind].valor = self.nuevo_val
             cels[self.ind].anot.set(text = cels[self.ind].valor)
         else: 
             cels = env.vars['seg_t'].arreglo.celdas
             lc = self.ind*2
             rc = self.ind*2 + 1
-            cels[self.ind].circ.set(facecolor= 'green')
-            cels[self.ind].valor = cels[lc].valor + cels[rc].valor
+            cels[self.ind].circ.set(facecolor= '#009999')
+            cels[self.ind].valor = min(cels[lc].valor,cels[rc].valor)
             cels[self.ind].anot.set(text = cels[self.ind].valor)
+        inds = self.indices_bajo(self.ind)
+        n = len(env.vars['seg_t'].tree)//2
+        text = "El nuevo nodo coloreado tiene valor {}.\n".format(cels[self.ind].valor)
+        text += "el minimo de los valores de sus hijos izquierdo y derecho.\n"
+        text += "El valor del nodo es el minimo del subarbol cuyas hojas abarcan el \n"
+        text += "rango de indices [{},{}]".format(inds[0] - n,inds[-1] - n)
+        x1,y1 = env.vars['seg_t'].arreglo.celdas[inds[0]].rect.get_xy() 
+        x2,y2  = env.vars['seg_t'].arreglo.celdas[inds[-1]].rect.get_xy() 
+        if(self.linea == None): 
+            self.linea = self.poner_linea( y1 - 2,x1,x2+2)
+            env.vars['ax'].relim()
+            env.vars['ax'].autoscale_view()
+        else: 
+            self.modificar_linea(x1,x2+2)
+        self.anot.set(text = text)
         self.ind = self.ind // 2 
     @out1.capture()
     def handler_teclas_ej(self,event): 
@@ -287,19 +327,68 @@ class Modificacion:
         elif(event.key == '+'): 
             self.zoom_mas() 
     def config_mouse(self): 
-        print("config mouse")
         env.vars['cid_m'] = env.vars['fig'].canvas.mpl_connect('button_press_event', self.handler_mouse)
-        print(env.vars['cid_m'])
     def config_teclas(self): 
         env.vars['cid_t'] = env.vars['fig'].canvas.mpl_connect('key_press_event', self.handler_tecla)
     def config_imagen(self): 
         plt.gca().set_aspect('equal', adjustable='box')
+        plt.subplots_adjust(bottom=0.3)
+        plt.axis("off")
+        self.ax_anot = plt.axes([0.1, 0.1, 0.8, 0.15])
+        plt.axis("off")
+        self.zoom_mas() 
+        self.zoom_mas() 
+    def indices_bajo(self,i): 
+        cola = [i]
+        hojas = [] 
+        n =  len(env.vars['seg_t'].tree)
+        while(cola): 
+            x = cola[0]
+            cola.pop(0)
+            if(2*x < n):
+                cola.append(2*x)
+                cola.append(2*x + 1)
+            else: 
+                hojas.append(x)
+        return hojas
+    def init_anot(self): 
+        text= "Escoge alguna casilla haciendo click en ella. Introduce un numero\n"
+        text += "en el cuadro de texto bajo la imagen y presiona el boton cuanto estes listo."
+        self.anot = self.ax_anot.text(0.1,0.7,text,va = 'top',ha = "left")
+    @out1.capture() 
+    def handler_listo(self,event): 
+        if(self.hoja == None): 
+            text = "Tienes que presionar una celda para poder ejecutar el algoritmo."
+            self.anot.set(text = text)     
+        else: 
+            self.nuevo_val = self.botones.children[0].value
+            self.botones.children =[] 
+            env.vars['fig'].canvas.mpl_disconnect(env.vars['cid_m']) 
+            env.vars['cid_t'] = env.vars['fig'].canvas.mpl_connect('key_press_event', self.handler_teclas_ej)
+            env.vars['seg_t'].arreglo.celdas[self.hoja].rect_anot.set(text = "${}$".format(self.nuevo_val))
+            text = "Haz click en la imagen, desde aqui cada vez que presiones n \n"
+            text += "se ejecutara el paso siguiente del algoritmo."
+            self.anot.set(text = text)
+    def poner_botones(self):  
+        t = widgets.IntText(
+                        value=0,
+                        description='numero:',
+                        disabled=False
+                        )
+        b = widgets.Button(description="Listo")
+        b.on_click(self.handler_listo)
+        if(self.botones == None): 
+            self.botones = widgets.VBox([t,b])
+            display(self.botones)
+        else: 
+            self.botones.children = [t,b]
     def __init__(self): 
         dibujar_arbol(env.vars['inp_arr']) 
         self.config_imagen()
         self.desactivar_letras_gui() 
         self.config_mouse() 
-        self.config_teclas()
+        self.init_anot() 
+        self.poner_botones() 
     def desactivar_letras_gui(self): 
         plt.rcParams["keymap.home"] = [] 
         plt.rcParams["keymap.back"] = [] 
@@ -320,7 +409,7 @@ env = Env()
 env.vars['fig'],env.vars['ax'] = plt.subplots()  
 env.vars['seg_t'] = None
 env.vars['rad'] = 1
-env.vars['inp_arr'] = [random.randint(1,10) for i in range(0,32) ]
+env.vars['inp_arr'] = [random.randint(1,10) for i in range(0,16) ]
 env.vars['cid_m'] = None
 env.vars['cid_t'] = None
 env.vars['tecla_f'] = None
